992c4306d20ba73342fb9f9f59dc4f437c8ec56a
aroncontreras9551-max <aroncontreras9551@gmail.com>
2025-12-24 19:52:25 +0500

    Create check-ip.js

---
+++ api/check-ip.js
@@ -0,0 +1,25 @@
+export default function handler(req, res) {
+  // List your allowed IPs here
+  const allowedIPs = [
+    '119.159.172.227', // Your IP
+    // Add more IPs: '203.0.113.45',
+  ];
+
+  // Get the user's IP address from the request
+  const userIP = req.headers['x-forwarded-for'] || 
+                 req.headers['x-real-ip'] || 
+                 req.connection.remoteAddress;
+
+  // Check if the IP is allowed
+  if (allowedIPs.includes(userIP)) {
+    res.status(200).json({ 
+      allowed: true, 
+      message: 'Access granted' 
+    });
+  } else {
+    res.status(403).json({ 
+      allowed: false, 
+      message: 'Access denied from this IP address.' 
+    });
+  }
+}
